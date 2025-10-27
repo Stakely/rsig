a4b811c09e45c55fe51a0b19b7febf11a056aa87
Pedro Carnerero <pedrocarnereromartinez@gmail.com>
2025-10-27 16:54:40 +0100

    feat: check database connection during server bootstrap
    Now that we've started working with the database, we've added a quick
    check when starting the HTTP server to verify that we have a connection
    to the database. If database is not reachable, we throw an error and
    prevent the server from starting, since database access will be
    essential for the correct behaviour of the application.

--- server/init.go
+++ server/init.go
@@ -1,9 +1,14 @@
 package server
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"net/http"
 	"rsig/internal/config"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
 func InitServer(cfg config.Config) error {
@@ -11,6 +16,18 @@ func InitServer(cfg config.Config) error {
 	mux := http.NewServeMux()
 	registerRoutes(mux)
 	fmt.Println("Rsig listening on", addr)
+
+	db, err := sql.Open("postgres", cfg.DATABASE.DbDsn)
+	if err != nil {
+		return fmt.Errorf("sql.Open failed: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return fmt.Errorf("database not reachable: %w", err)
+	}
 
 	return http.ListenAndServe(addr, mux)
 }
